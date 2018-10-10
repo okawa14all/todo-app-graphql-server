01ef5e20e88cc6dac04b04fce49f2d97fb2021a0
大川仁志 <hitoshi.okawa.biz@gmail.com>
2018-10-10 09:40:04 +0900

    Add todos

--- src/index.js
+++ src/index.js
@@ -3,12 +3,40 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  todos: [Todo!]!
+}
+
+type Todo {
+  id: ID!
+  title: String!
+  completed: Boolean!
 }
 `
+
+let todos = [
+  {
+    id: 'todo1',
+    title: '買い物に行く',
+    completed: true,
+  },
+  {
+    id: 'todo2',
+    title: 'GraphQLの勉強',
+    completed: false,
+  },
+]
+
 const resolvers = {
   Query: {
-    info: () => 'hoge'
+    info: () => 'hoge',
+    todos: () => todos,
   },
+
+  Todo: {
+    id: (root) => root.id,
+    title: (root) => root.title,
+    completed: (root) => root.completed,
+  }
 }
 
 const server = new GraphQLServer({
